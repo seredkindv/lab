ab77994b47600d1b2aecb4888678d72105e63f3d
vdsered <vdsered@gmail.com>
2021-01-27 15:05:49 +0700

    Создание проект (1)

---
+++ daniil/daniil/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace daniil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
